d799a7ee62818c57e5f26732f267017fd6535a98
Michał Moskała <micmos@azimo.com>
2017-03-12 19:23:58 +0100

    Moongose dependency added

--- app.js
+++ app.js
@@ -1,14 +1,39 @@
 var express = require('express')
+var mongoose = require('mongoose')
 var app = express()
+
+mongoose.connect('mongodb://localhost/saver')
+
+var spendingSchema = new mongoose.Schema({
+    name: String,
+    value: Number,
+});
+
+var Spending = mongoose.model('Spending', spendingSchema)
 
 app.get('/', function(request, response) {
     response.send('Hello World!')
 })
 
-app.get('/jsonTest', function(request, response){
-    response.json({title:'Item1'})
+app.get('/insertSpending', function(request, response){
+    var spendingToInsert = new Spending({name: "Bialka", value: 800})
+    spendingToInsert.save(function(err){
+        if (err) {
+            console.log(err)
+        } else {
+            console.log(spendingToInsert)
+        }
+    })
+    response.send('OK')
+})
+
+app.get('/allSpendings', function(request, response){
+    Spending.find(function(err, spendings){
+        response.json(spendings)
+    })
 })
 
 app.listen(3000, function() {
     console.log('App is listening on port 3000')
 })
+
