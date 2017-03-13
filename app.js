fe1f2a3ed93a196a31fb30f13a19fc4f40c5d917
Michał Moskała <moskala.michal@gmail.com>
2017-03-13 21:02:43 +0100

    Using body-parser express.js

--- app.js
+++ app.js
@@ -1,39 +1,31 @@
 var express = require('express')
+var bodyParser = require('body-parser')
 var mongoose = require('mongoose')
 var app = express()
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 mongoose.connect('mongodb://localhost/saver')
 
-var spendingSchema = new mongoose.Schema({
-    name: String,
+var transactionSchema = new mongoose.Schema({
+    identifier: Number,
+    title: String,
     value: Number,
-});
-
-var Spending = mongoose.model('Spending', spendingSchema)
-
-app.get('/helloWorld', function(request, response) {
-    response.send('Hello World!')
+    category: String
 })
 
-app.get('/insertSpending', function(request, response){
-    var spendingToInsert = new Spending({name: "Bialka", value: 800})
-    spendingToInsert.save(function(err){
-        if (err) {
-            console.log(err)
-        } else {
-            console.log(spendingToInsert)
-        }
-    })
-    response.send('OK')
-})
+var Transaction = mongoose.model('Transaction', transactionSchema)
 
-app.get('/allSpendings', function(request, response){
-    Spending.find(function(err, spendings){
-        response.json(spendings)
-    })
+app.put('/transaction', function(request, response){
+    var title = request.body.title
+    var value = request.body.value
+    var category = request.body.category
+    console.log(request.body)
+    console.log(request.headers)
+    response.send('a')
 })
 
 app.listen(3000, function() {
     console.log('App is listening on port 3000')
 })
-
